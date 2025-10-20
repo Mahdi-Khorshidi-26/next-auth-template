1d20e8f4a9b67d70be21d6fca281aa29a7080e3b
Mahdi-Khorshidi-26 <khorshidi.mahdi.1998@gmail.com>
2025-10-20 16:38:16 +0330

    creating some migration .

--- commands.ts
+++ commands.ts
@@ -1,3 +1,25 @@
 //1- To create the .env.local folder with the authentication secret run the following command :
 // npx auth secret
 
+//2- For starter of database with prisma we have this :
+// npx prisma init
+
+//3- For creating the database migration we have this :
+// npx prisma migrate dev --name init
+
+//4- To install the prisma client we have this :
+// npx prisma generate
+
+//5- For production database migration we have this :
+// npx prisma migrate deploy
+
+//6- To open the prisma studio we have this :
+// npx prisma studio
+
+//7- To install the required dependencies we have this :
+// npm install @auth/prisma-adapter @auth/next-auth next-auth @prisma/client prisma
+
+//8- To run the development server we have this :
+// npm run dev
+
+// Make sure to set up a GitHub OAuth App and add the Client ID and Client Secret to your .env.local file as AUTH_GITHUB_ID and AUTH_GITHUB_SECRET respectively.
