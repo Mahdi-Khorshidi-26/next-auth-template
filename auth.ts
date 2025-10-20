ffb9cfcc4fadfa79076d415d2d732027dac45bc3
Mahdi-Khorshidi-26 <khorshidi.mahdi.1998@gmail.com>
2025-10-20 16:48:51 +0330

    adding shadcn to the project .

--- auth.ts
+++ auth.ts
@@ -2,8 +2,47 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import { prisma } from "./lib/prisma";
+import Credentials from "next-auth/providers/credentials";
+import { compare } from "bcrypt";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      session.user.id = token.id as string;
+      return session;
+    },
+  },
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email as string },
+        });
+        if (!user) {
+          throw new Error("No user found with the given email");
+        } else {
+          const passwordCorrect = await compare(
+            credentials.password as string,
+            user.password as string
+          );
+          if (!passwordCorrect) {
+            throw new Error("Incorrect password");
+          }
+        }
+        return { id: user.id.toString(), email: user.email };
+      },
+    }),
+    GitHub,
+  ],
   adapter: PrismaAdapter(prisma),
 });
